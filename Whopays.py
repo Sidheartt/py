74c24d0ae37013bf609e38b1c7cb1553fd1377bd
Siddharth <siddharth.desai1996@gmail.com>
2021-01-16 01:27:36 +0530

    use this prog to see who'll pay amongst us

---
+++ Whopays.py
@@ -0,0 +1,8 @@
+#Who pays the bill
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+name = random.choice(names)
+print(name)
+
