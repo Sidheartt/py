7abb05ac3a84aae5d6c346b4bb2aa114ff9d31ea
Siddharth Desai <75911033+Sidheartt@users.noreply.github.com>
2021-07-25 23:39:38 +0530

    initial commit

---
+++ np_bool.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = np.arange(1,11)
+
+arr
+
+array([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+
+bool_5 = arr > 5
+
+bool_5
+
+array([False, False, False, False, False,  True,  True,  True,  True,
+        True])
+		
+		
